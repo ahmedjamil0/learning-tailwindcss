83184425f999f752cfe748aa34f39c344de49146
Ahmed Jamil <ahmed.jamil@alphasquadinc.com>
2021-03-05 17:06:26 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        amazing : "#B30057",
+        wow:{
+          100: "#040A31",
+          200: "#040A35",
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
